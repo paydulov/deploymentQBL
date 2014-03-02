0418f21cc023fe9050222801dfd2c2536b5442c0
paydulov <paydulov@hotmail.com>
2014-03-02 12:50:38 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,14 +10,14 @@ chkconfig httpd on
 service httpd start
 yum -y install php php-common php-gd php-mysql php-xml php-mbstring
 service httpd restart
-yum -y install mysql mysql-server
+yum -y install mysql mysql-server mysql-connector-java
 chkconfig mysqld on
 service mysqld start
 mysqladmin -u root password 'password'
 mkdir /vagrant/petclinic
 cd /vagrant/petclinic
 mkdir www logs tmp
-cd /vagrant/
+cd /vagrant
 wget https://dl.dropboxusercontent.com/u/9937469/petclinic.conf
 cp /vagrant/petclinic.conf /etc/httpd/conf.d/
 service httpd restart
